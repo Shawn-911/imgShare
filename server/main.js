cd7a2f5c06429724087b5ca1acd967a44a5f1e7e
Shawn-911 <ashawnmichael@gmail.com>
2020-01-31 13:47:52 -0400

    added data base

--- server/main.js
+++ server/main.js
@@ -1,5 +1,17 @@
 import { Meteor } from 'meteor/meteor';
+import '../lib/collection.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
-});
+  if(imagesdb.find().count()<1)
+  {
+  	for (var i =1; i <= 25; i++) {
+  		console.log(i);
+  		imagesdb.insert({
+  			"path":"img_"+i+".jpg",
+  			"title":"title"+i,
+  			"desc":"desc"+i
+  		});
+  	}
+  }
+})
